2f502ada986c4368534a7f55206ceb43ce33d6fd
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2022-04-15 06:42:38 +0300

    Menu.cs

---
+++ SharedModelSample/Models/Menu.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Yazi { get; set; }
+        public string Url { get; set; }
+    }
+}
